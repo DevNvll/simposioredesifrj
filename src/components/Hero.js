c32fee7c316edb7c2ab6c6206590eca9e8497956
Henrick Mello <hmartins505@gmail.com>
2020-03-20 19:17:17 -0300

    fix hero responsiveness

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,16 +3,16 @@ import wave from "../assets/wave.svg"
 
 export function Hero(props) {
   return (
-    <div className="relative flex flex-col-reverse items-center justify-center min-h-screen pt-5 pb-10 bg-gray-900 md:bg-white md:flex-row md:min-h-0 md:pt-20">
-      <div className="container relative z-20 flex flex-col-reverse items-center justify-between md:flex-row">
-        <div className="flex flex-col items-center w-full px-4 text-center md:items-start md:w-auto md:px-0 md:text-left">
-          <h1 className="w-full text-3xl font-bold text-gray-300 md:text-4xl md:w-4/5">
+    <div className="relative flex flex-col-reverse items-center justify-center min-h-screen pt-5 pb-10 bg-gray-900 xl:bg-white xl:flex-row xl:min-h-0 xl:pt-20">
+      <div className="container relative z-20 flex flex-col-reverse items-center justify-between xl:flex-row">
+        <div className="flex flex-col items-center w-full px-4 text-center xl:items-start xl:w-auto xl:px-0 xl:text-left">
+          <h1 className="w-full text-3xl font-bold text-gray-300 xl:text-4xl xl:w-4/5">
             1º Simpósio de Redes da Região dos Lagos
           </h1>
-          <h2 className="mt-4 text-lg text-gray-500 md:text-xl">
+          <h2 className="mt-4 text-lg text-gray-500 xl:text-xl">
             8 e 9 de Outubro no IFRJ Campus Arraial do Cabo
           </h2>
-          <div className="flex flex-col justify-center mt-8 md:flex-row xl:mt-12 xl:justify-start">
+          <div className="flex flex-col justify-center mt-8 xl:flex-row xl:mt-12 xl:justify-start">
             <button className="px-6 py-4 font-bold text-white transition duration-200 ease-in-out rounded shadow bg-primary-400 hover:bg-primary-300">
               Inscreva-se
             </button>
@@ -23,11 +23,11 @@ export function Hero(props) {
         </div>
         <img
           src={heroImage}
-          className="w-64 md:-mr-20"
+          className="w-64 xl:-mr-20"
           style={{ width: "700px" }}
         />
       </div>
-      <img src={wave} className="absolute z-10 md:top-0 " />
+      <img src={wave} className="absolute z-10 xl:top-0 " />
     </div>
   )
 }
