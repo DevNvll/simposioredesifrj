2cc5cc75b23c27dda8c54466be960d6abd654a80
Henrick Mello <hmartins505@gmail.com>
2020-03-20 19:15:08 -0300

    added menu to mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,25 @@ import logo from "../assets/img/logo-ifrj.png"
 import { useRouter } from "next/router"
 import Link from "next/link"
 import cx from "classnames"
+import { useState } from "react"
 
 function NavItem({ href, label, ...props }) {
   const router = useRouter()
   const active = router.pathname
   return (
     <li
-      className={cx("ml-6 md:ml-8 text-primary-500 font-medium", {
-        "text-primary-700 ": active === href
-      })}
+      className={cx(
+        "xl:ml-6 xl:ml-8 text-primary-500 font-medium mt-4 md:mt-0",
+        {
+          "text-primary-700 ": active === href
+        },
+        "hover:bg-gray-100 sm:rounded-md p-2"
+      )}
     >
       <Link href={href}>
         <a
           className={cx({
-            "pb-2 border-solid border-b border-primary-500": active === href
+            "pb-2 border-solid md:border-b border-primary-500": active === href
           })}
         >
           {label}
@@ -26,14 +31,76 @@ function NavItem({ href, label, ...props }) {
 }
 
 export function Navbar({ location, ...props }) {
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <header className="fixed top-0 z-50 flex justify-center w-full p-4 bg-white border-t-4 border-solid shadow border-secondary-400">
       <nav className="container flex justify-between">
         <div className="flex items-center content-center">
           <img className="h-16" src={logo} alt="Logo Instituto Federal" />
         </div>
-        <div className="flex">
-          <ul className="items-center justify-between hidden md:flex">
+        <div className="flex flex-col justify-center md:flex-row">
+          <ul className="items-center justify-between hidden xl:flex">
+            <NavItem href="/" label="Início" />
+            <NavItem href="/programacao" label="Programação" />
+            <NavItem href="/submissoes" label="Submissões" />
+            <NavItem href="/incricoes" label="Pré-Inscrições" />
+            <NavItem href="/contato" label="Fale com a Organização" />
+          </ul>
+          <button
+            onClick={() => {
+              setMenuOpen(true)
+            }}
+            type="button"
+            className="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md xl:hidden hover:text-primary-300 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-primary-300"
+          >
+            <svg
+              className="w-6 h-6"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+      </nav>
+      <div
+        className={cx("fixed xl:hidden top-0 w-full h-64 p-4", {
+          hidden: !menuOpen
+        })}
+      >
+        <div className="flex flex-col w-full h-auto bg-white rounded-lg shadow-lg">
+          <div className="flex items-center justify-between px-5 pt-4 ">
+            <img className="h-8" src={logo} alt="Logo Instituto Federal" />
+
+            <button
+              onClick={() => {
+                setMenuOpen(false)
+              }}
+              type="button"
+              className="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-primary-300 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-primary-300"
+            >
+              <svg
+                className="w-6 h-6"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <ul className="m-4 mt-4">
             <NavItem href="/" label="Início" />
             <NavItem href="/programacao" label="Programação" />
             <NavItem href="/submissoes" label="Submissões" />
@@ -41,7 +108,7 @@ export function Navbar({ location, ...props }) {
             <NavItem href="/contato" label="Fale com a Organização" />
           </ul>
         </div>
-      </nav>
+      </div>
     </header>
   )
 }
